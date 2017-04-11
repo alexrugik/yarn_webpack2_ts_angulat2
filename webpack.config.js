d1216608587c34335e486c6f867ee13f09d5a186
Alex Ruzhinskiy <alexruzhinskiy@gmail.com>
2017-04-11 15:24:01 +0300

    added base webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,57 @@ module.exports = {
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'dist/src')
-    }
+        path: path.resolve(__dirname, 'dist/src'),
+        publicPath: '/dist/'
+    },
+    module: {
+        rules: []
+    },
+    resolve: {
+        modules: [
+            'node_modules',
+            path.resolve(__dirname, 'app')
+        ],
+        extensions: ['.js', '.json', '.css'],
+    },
+    performance: {
+        hints: 'warning',
+        maxAssetSize: 200000,
+        maxEntrypointSize: 400000,
+        assetFilter: function (assetFilename) {
+            return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+        }
+    },
+    devtool: 'source-map',
+    context: __dirname,
+    stats: "errors-only",
+    devServer: {
+        port: 7777,
+        contentBase: path.join(__dirname, 'dist'),
+        publicPath: '/dist/',
+        compress: true,
+        historyApiFallback: true,
+        hot: true,
+        https: false,
+        noInfo: true,
+        inline: true,
+        stats: 'errors-only',
+    },
+    plugins: [],
+    profile: true, // boolean
+    // capture timing information
+    bail: true, //boolean
+    // fail out on the first error instead of tolerating it.
+    cache: false,
+    // disable/enable caching
+    watchOptions: {
+        aggregateTimeout: 1000, // in ms
+        // aggregates multiple changes to a single rebuild
+
+        poll: true,
+        poll: 500, // intervall in ms
+        // enables polling mode for watching
+        // must be used on filesystems that doesn't notify on change
+        // i. e. nfs shares
+    },
 };
